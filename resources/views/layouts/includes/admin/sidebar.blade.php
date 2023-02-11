d1efacd921cbd1189ed5bdcd8faac16fe9a34fcd
trieuconcrete <trieunb@concrete-corp.com>
2023-02-11 13:11:14 +0900

    init project

---
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -0,0 +1,77 @@
+<!-- partial:partials/_sidebar.html -->
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+<ul class="nav">
+    <li class="nav-item">
+    <a class="nav-link" href="{{ route('admin.dashboard') }}">
+        <i class="mdi mdi-home menu-icon"></i>
+        <span class="menu-title">Dashboard</span>
+    </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="mdi mdi-sale menu-icon"></i>
+            <span class="menu-title">Sales</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#category" aria-expanded="false" aria-controls="ui-basic">
+            <i class="mdi mdi-view-list menu-icon"></i>
+            <span class="menu-title">Category</span>
+            <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="category">
+            <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="#">Add</a></li>
+            <li class="nav-item"> <a class="nav-link" href="#">List</a></li>
+            </ul>
+        </div>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#product" aria-expanded="false" aria-controls="ui-basic">
+            <i class="mdi mdi-plus-circle menu-icon"></i>
+            <span class="menu-title">Products</span>
+            <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="product">
+            <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
+            </ul>
+        </div>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="pages/forms/basic_elements.html">
+            <i class="mdi mdi-view-headline menu-icon"></i>
+            <span class="menu-title">Brands</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#user" aria-expanded="false" aria-controls="auth">
+            <i class="mdi mdi-account-multiple-plus menu-icon"></i>
+            <span class="menu-title">Users</span>
+            <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="user">
+            <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/login-2.html"> Login 2 </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/register-2.html"> Register 2 </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/lock-screen.html"> Lockscreen </a></li>
+            </ul>
+        </div>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="documentation/documentation.html">
+            <i class="mdi mdi-animation menu-icon"></i>
+            <span class="menu-title">Home Slider</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="documentation/documentation.html">
+            <i class="mdi mdi-cogs menu-icon"></i>
+            <span class="menu-title">Site Setting</span>
+        </a>
+    </li>
+</ul>
+</nav>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->name('admin.')->middleware(['auth', 'isAdmin'])->group(function () {
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
