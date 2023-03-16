b8ce887f0b427fe15deffbaf9d49331fed179f38
trieuconcrete <trieunb@concrete-corp.com>
2023-03-16 10:36:22 +0900

    add cicd

--- config/app.php
+++ config/app.php
@@ -185,7 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
-        // Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
