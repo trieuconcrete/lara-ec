23d5d69a895ce4d1d56342c96ee457a82e31df2d
trieuconcrete <trieunb@concrete-corp.com>
2023-04-01 11:41:17 +0900

    add livewire ui dashboard

---
+++ app/helpers.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!function_exists('greeting')) {
+    function greeting()
+    {
+        date_default_timezone_set(config('app.timezone'));
+        $hour = date('H', time());
+        
+        if ($hour > 4 && $hour <= 11) {
+            echo "Good Morning";
+        } else if ($hour > 11 && $hour <= 16) {
+            echo "Good Afternoon";
+        } else if ($hour > 16 && $hour <= 23) {
+            echo "Good Evening";
+        } else {
+            echo "Why aren't you asleep?  Are you programming?";
+        }
+    }
+}
